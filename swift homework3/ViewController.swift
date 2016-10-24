e297c2493df8999f80fc51cc8c114511a46fb671
PICKACHU <PICKACHU@pickachudemacbook-pro.local>
2016-10-24 10:02:42 +0800

    Initial Commit

---
+++ swift homework3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift homework3
+//
+//  Created by PICKACHU on 2016/10/24.
+//  Copyright © 2016年 PICKACHU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
